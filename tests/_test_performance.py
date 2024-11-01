d984b207040c1af9e6af5ecd28464c28734e03cc
kitsudo1412 <kitsudo1412@gmail.com>
2024-11-01 20:24:46 +0100

    add performance test

--- tests/_test_performance.py
+++
@@ -1,8 +0,0 @@
-import time
-import LKHpy as LK
-
-def test_performance():
-    start = time.time()
-    solution = LK.par_file('data/whizzkids96.par')
-    secondsTaken = time.time() - start
-    assert secondsTaken < 10, f"Expected less than 10 seconds, got {secondsTaken}"
