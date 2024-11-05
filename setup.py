210f55e0738a13b91afec70549b304e69aa2fee4
kitsudo1412 <kitsudo1412@gmail.com>
2024-11-05 21:56:43 +0100

    release 0.0.7

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="LKHpy",
-    version="0.0.6",
+    version="0.0.7",
     author="Tuan Anh Dao",
     author_email="dta.hust@gmail.com",
     description="LKHpy",
