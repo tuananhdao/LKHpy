bbb8734a3e7ac61d6e2f1fc7be214696608eadf5
kitsudo1412 <kitsudo1412@gmail.com>
2024-11-13 21:07:50 +0100

    add missing package

--- setup.py
+++ setup.py
@@ -135,6 +135,6 @@ setup(
     ext_modules=[CMakeExtension("LKHpy")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
-    extras_require={"test": ["pytest>=6.0","numpy>=1.20"]},
+    extras_require={"test": ["pytest>=6.0","numpy>=1.20","requests>=2.32"]},
     python_requires=">=3.8",
 )
