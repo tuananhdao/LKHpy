e4d737931e5caa5c2301b4c03fbb9a75a1c72221
SimonCao1207 <hainamkaist@gmail.com>
2024-11-19 18:33:36 +0700

    apply patch "Caching system improvement"

--- LKH-3.0.12/SRC/ReadProblem.c
+++ LKH-3.0.12/SRC/ReadProblem.c
@@ -437,7 +437,7 @@ void ReadProblem()
     Type = EdgeWeightType = EdgeWeightFormat = 0;
     EdgeDataFormat = NodeCoordType = DisplayDataType = 0;
     Distance = 0;
-    C = 0;
+    _C = 0;
     c = 0;
     while ((Line = ReadLine(ProblemFile))) {
         if (!(Keyword = strtok(Line, Delimiters)))
@@ -796,7 +796,13 @@ void ReadProblem()
                         NodeSet[i].C[j] += NodeSet[i].ServiceTime;
         }
     }
+
+#ifdef CAVA_CACHE
+    _C = WeightType == EXPLICIT ? C_EXPLICIT : C_FUNCTION;
+#else
     C = WeightType == EXPLICIT ? C_EXPLICIT : C_FUNCTION;
+#endif  
+
     D = WeightType == EXPLICIT ? D_EXPLICIT : D_FUNCTION;
     if (ProblemType != CVRP && ProblemType != CVRPTW &&
         ProblemType != CTSP && ProblemType != STTSP &&
